3779a4007ec6dc5de5431fdb141c7486d8f4eacc
nishchala <28265373+nishchala@users.noreply.github.com>
2022-04-08 17:14:50 -0400

    "created component files and imported in App.js"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import ScrollToTop from "./components/ScrollToTop";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import Portfolio from "./components/Portfolio";
+import Reviews from "./components/Reviews";
+import Products from "./components/Products";
+import Newsletter from "./components/Newsletter";
+import Footer from "./components/Footer";
 
-const App = () => {
+export default function App() {
   return (
-    <div>Hello World</div>
+    <>
+      <ScrollToTop />
+      <Navbar />
+      <Hero />
+      <Services />
+      <Portfolio />
+      <Reviews />
+      <Products />
+      <Newsletter />
+      <Footer />
+    </>
   )
 }
-
-export default App
